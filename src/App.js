9bff06a08c609043cf586c3af835607de9210e28
Kekichka <alonadaniluk695@gmail.com>
2024-11-21 23:00:32 +0200

    pizdets

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 import AboutAuthors from './components/AboutAuthors';
 import InterestingFacts from './components/InterestingFacts';
 import TestChooser from './components/TestChooser';
+import Taras from './components/Taras';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/about-authors" element={<AboutAuthors />} />
           <Route path="/interesting-facts" element={<InterestingFacts />} />
           <Route path="/tests" element={<TestChooser />} />
+          <Route path="/about-authors/taras-shevchenko" component={Taras} />
         </Routes>
       </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,9 @@ const Header = () => {
           <li className="dropdown">
             <Link to="/about-authors">Біографії</Link>
             <ul className="dropdown-menu">
-              <li><Link to="/about-authors/author1">Тарас Шевченко</Link></li>
-              <li><Link to="/about-authors/author2">Леся Українка</Link></li>
-              <li><Link to="/about-authors/author3">Іван Франко</Link></li>
+              <li><Link to="/about-authors/taras-shevchenko">Тарас Шевченко</Link></li>
+              <li><Link to="/about-authors/lesya-ukrainka">Леся Українка</Link></li>
+              <li><Link to="/about-authors/ivan-franko">Іван Франко</Link></li>
             </ul>
           </li>
           <li><Link to="/interesting-facts">Цікаві факти</Link></li>
